537ba85002bd75c5f30244e727fce8f43323c936
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-07-12 09:13:03 -0500

    (feat): write hit and isSunk methods for Ship class.

--- src/Ship.js
+++ src/Ship.js
@@ -2,6 +2,15 @@ class Ship {
     constructor (length) {
         this.length = length;
         this.hits = 0;
+        this.coordinates = [];
+    }
+
+    hit () {
+        this.hits++;
+    }
+
+    isSunk () {
+        return this.hits >= this.length ? true : false;
     }
 }
 
--- src/Ship.test.js
+++ src/Ship.test.js
@@ -16,4 +16,22 @@ describe("Ship module", () => {
             const expectedLength = 5;
             expect(s.length).toBe(expectedLength);
     });
+
+    test("Should have a hit() method that increases number of hits", () => {
+        const originalHits = s.hits;
+        s.hit();
+        expect(s.hits).toBe(originalHits + 1);
+    });
+
+    test("Has a isSunk() method that returns false if number of hits is less than length", () => {
+        expect(s.isSunk()).toBe(false);
+    });
+
+    test("isSunk() method returns true if hits is greater than or equal to length", () => {
+        for (let i = 0; i < 5; i++) {
+            s.hit();
+        }
+
+        expect(s.isSunk()).toBe(true);
+    });
 });
