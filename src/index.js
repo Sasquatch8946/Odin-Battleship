83d72d192d7898be679265b099a15a41f0be03ea
Sasquatch8946 <42752821+Sasquatch8946@users.noreply.github.com>
2025-07-14 19:25:47 -0500

    Import modules into index.js and start writing a main function that creates two players with two boards and places the ships automatically.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './styles.css';
+import Gameboard from './Gameboard';
+import Player from './Player';
+import Ship from './Ship';
+
+
+function main () {
+    const player1 = new Player();
+    const player2 = new Player();
+    player1.gameboard.placeShip([0, 0], [1, 0]);
+    player1.gameboard.placeShip([0, 2], [0, 5]);
+    player1.gameboard.placeShip([3, 0], [3, 3]);
+    player1.gameboard.placeShip([4, 9], [9, 9]);
+    player1.gameboard.placeShip([5, 9], [7, 9]);
+
+    player2.gameboard.placeShip([4, 5], [8, 5]);
+    player2.gameboard.placeShip([5, 2], [8, 2]);
+    player2.gameboard.placeShip([9, 2], [9, 3]);
+    player2.gameboard.placeShip([1, 8], [6, 8]);
+    player2.gameboard.placeShip([7, 5], [7, 7]);
+}
